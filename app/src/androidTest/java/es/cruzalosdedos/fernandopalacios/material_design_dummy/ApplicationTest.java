6a62533bb11df96a4b41a5dd3fc89f593f6045e1
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-12-29 17:36:15 +0100

    initial commit, Android Studio proyect default settings

---
+++ app/src/androidTest/java/es/cruzalosdedos/fernandopalacios/material_design_dummy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package es.cruzalosdedos.fernandopalacios.material_design_dummy;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

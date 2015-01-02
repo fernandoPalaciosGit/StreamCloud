aa92ccab2b31aa37773b8d68069e0665c6c4a68a
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-01-02 21:52:23 +0100

    destacados and amigos fragments, portrait and landscape view

--- app/src/main/java/es/cruzalosdedos/fernandopalacios/StreamSound/fragments/Amigos.java
+++ app/src/main/java/es/cruzalosdedos/fernandopalacios/StreamSound/fragments/Amigos.java
@@ -1,23 +1,24 @@
 package es.cruzalosdedos.fernandopalacios.StreamSound.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import es.cruzalosdedos.fernandopalacios.StreamSound.R;
 
-public class Destacados extends Fragment {
+public class Amigos extends Fragment {
 
-    public Destacados() {
-
+    public Amigos() {
+        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_destacados, container, false);
+        
+        return inflater.inflate(R.layout.fragment_amigos, container, false);
     }
+
 }

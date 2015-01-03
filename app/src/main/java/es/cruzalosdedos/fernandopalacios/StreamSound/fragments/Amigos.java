1893b1685ab9dabe0ccac32b3bd64ed25c574f6b
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-01-03 13:39:19 +0100

    create cardview friends

--- app/src/main/java/es/cruzalosdedos/fernandopalacios/StreamSound/fragments/Amigos.java
+++ app/src/main/java/es/cruzalosdedos/fernandopalacios/StreamSound/fragments/Amigos.java
@@ -18,7 +18,7 @@ public class Amigos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         
-        return inflater.inflate(R.layout.fragment_amigos, container, false);
+        return inflater.inflate(R.layout.fragment_friends, container, false);
     }
 
 }

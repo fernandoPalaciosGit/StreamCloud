d040e59cd9b91c96a1b778ffe43bede76bc26ed4
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-01-03 11:14:00 +0100

    create Friend model class

---
+++ app/src/main/java/es/cruzalosdedos/fernandopalacios/StreamSound/models/Friend.java
@@ -0,0 +1,34 @@
+package es.cruzalosdedos.fernandopalacios.StreamSound.models;
+
+/**
+ * Created by Fernando Palacios on 03/01/2015.
+ */
+public class Friend {
+    private String name;
+    private String twitter;
+    private String lastSong;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getLastSong() {
+        return lastSong;
+    }
+
+    public void setLastSong(String lastSong) {
+        this.lastSong = lastSong;
+    }
+}
